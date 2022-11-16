b620c4bf33d3a374c37c5504abd324ee36b7853b
Angel Pérez <angel.perez.munoz@alumnos.upm.es>
2022-11-16 16:55:56 +0100

    Print error message if socat is not installed

--- scripts/fake-serial-port.sh
+++ scripts/fake-serial-port.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Note that the ACS_PIL communicates through TTY_HOST
+#       and the ACS algorithm running in TASTE though TTY_TARGET
 TTY_HOST=/dev/ttyUSB0
 TTY_TARGET=/dev/ttyOBC
 
-# Note that the ACS_PIL communicates through TTY_HOST
-#       and the ACS algorithm running in TASTE though TTY_TARGET
+# Check that socat is installed
+command -V socat > /dev/null || exit "Install socat!"
 
+# Creates fake ttys
 sudo socat -d -d -v pty,rawer,mode=666,link=${TTY_HOST},b115200 \
                     pty,rawer,mode=666,link=${TTY_TARGET},b115200 \
-                    1> /dev/null
+                    &> /dev/null
